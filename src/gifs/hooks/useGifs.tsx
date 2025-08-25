5b2049e5514c5b8b0142b116493d484f2ca6f3d5
sorodriguezz <s.rodriguezzapata01@gmail.com>
2025-08-24 22:48:33 -0400

    feat: agregar hook useGifs para manejar la búsqueda y almacenamiento en caché de GIFs

---
+++ src/gifs/hooks/useGifs.tsx
@@ -0,0 +1,41 @@
+import { useState } from "react";
+import type { Gif } from "../interfaces/gif.interface";
+import { getGifsByQuery } from "../actions/get-gifs-by-query.action";
+
+const gifsCache: Record<string, Gif[]> = {};
+
+export const useGifs = () => {
+  const [previousTerms, setPreviousTerms] = useState<string[]>([]);
+  const [gifs, setGifs] = useState<Gif[]>([]);
+
+  const handleTermClicked = async (term: string) => {
+    if (gifsCache[term]) {
+      setGifs(gifsCache[term]);
+      return;
+    }
+    const gifs = await getGifsByQuery(term);
+    setGifs(gifs);
+  };
+
+  const handleSearch = async (query: string = "") => {
+    query = query.trim().toLowerCase();
+
+    if (query.length === 0) return;
+
+    if (previousTerms.includes(query)) return;
+
+    setPreviousTerms([query, ...previousTerms].slice(0, 8));
+
+    const gifs = await getGifsByQuery(query);
+    setGifs(gifs);
+
+    gifsCache[query] = gifs;
+  };
+
+  return {
+    previousTerms,
+    gifs,
+    handleTermClicked,
+    handleSearch,
+  };
+};
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import { GifsApp } from './GifsApp'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { GifsApp } from "./GifsApp";
+import "./index.css";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <GifsApp />
-  </StrictMode>,
-)
+    {/* <MyCounterApp /> */}
+  </StrictMode>
+);
