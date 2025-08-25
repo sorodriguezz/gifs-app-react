b8cc3d3cc8be6d918ac4cc2bc8843101c2b8785b
sorodriguezz <s.rodriguezzapata01@gmail.com>
2025-08-24 22:48:28 -0400

    feat: implement useCounter hook and MyCounterApp component

--- src/GifsApp.tsx
+++ src/GifsApp.tsx
@@ -1,32 +1,11 @@
-import { useState } from "react";
-import { getGifsByQuery } from "./gifs/actions/get-gifs-by-query.action";
 import { GifList } from "./gifs/components/GifList";
 import { PreviousSearches } from "./gifs/components/PreviousSearches";
+import { useGifs } from "./gifs/hooks/useGifs";
 import { CustomHeader } from "./shared/components/CustomHeader";
 import { SearchBar } from "./shared/components/SearchBar";
-import type { Gif } from "./gifs/interfaces/gif.interface";
 
 export const GifsApp = () => {
-  const [previousTerms, setPreviousTerms] = useState<string[]>([]);
-  const [gifs, setGifs] = useState<Gif[]>([]);
-
-  const handleTermClicked = (term: string) => {
-    console.log({ term });
-  };
-
-  const handleSearch = async (query: string = "") => {
-    query = query.trim().toLowerCase();
-
-    if (query.length === 0) return;
-
-    if (previousTerms.includes(query)) return;
-
-    setPreviousTerms([query, ...previousTerms].slice(0, 8));
-
-    const gifs = await getGifsByQuery(query);
-
-    setGifs(gifs);
-  };
+  const { handleSearch, handleTermClicked, previousTerms, gifs } = useGifs();
 
   return (
     <>
