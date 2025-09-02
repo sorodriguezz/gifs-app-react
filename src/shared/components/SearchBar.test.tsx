b8ddc9d91a74a82f5588aba908458b10cdf037f2
sorodriguezz <s.rodriguezzapata01@gmail.com>
2025-09-01 23:03:25 -0400

    Add code coverage reports and tests for SearchBar component
    - Generated HTML coverage reports for src/mock-data, src/shared/components/CustomHeader.tsx, src/shared/components/SearchBar.tsx, and src/shared/components/index.html.
    - Implemented unit tests for SearchBar component, covering rendering, input handling, and debounce functionality.
    - Created snapshot tests for SearchBar to ensure consistent rendering.
    - Updated coverage summary to reflect new test results and coverage metrics.

---
+++ src/shared/components/SearchBar.test.tsx
@@ -0,0 +1,73 @@
+import { fireEvent, render, screen, waitFor } from "@testing-library/react";
+import { describe, expect, test, vi } from "vitest";
+import { SearchBar } from "./SearchBar";
+
+describe("SearchBar", () => {
+  test("should render searchbar correctly", () => {
+    const { container } = render(
+      <SearchBar onQuery={() => {}} textButton={""} />
+    );
+
+    expect(container).toMatchSnapshot();
+    expect(screen.getByRole("textbox")).toBeDefined();
+    expect(screen.getByRole("button")).toBeDefined();
+  });
+
+  test("should call onQuery with the correct value after 700ms", async () => {
+    const onQuery = vi.fn();
+
+    render(<SearchBar onQuery={onQuery} textButton={""} />);
+
+    const input = screen.getByRole("textbox");
+    fireEvent.change(input, { target: { value: "test" } });
+
+    await waitFor(() => {
+      expect(onQuery).toHaveBeenCalled();
+      expect(onQuery).toHaveBeenCalledWith("test");
+    });
+  });
+
+  test("should call only once with the last value (debounce)", async () => {
+    const onQuery = vi.fn();
+
+    render(<SearchBar onQuery={onQuery} textButton={""} />);
+
+    const input = screen.getByRole("textbox");
+    fireEvent.change(input, { target: { value: "t" } });
+    fireEvent.change(input, { target: { value: "te" } });
+    fireEvent.change(input, { target: { value: "tes" } });
+    fireEvent.change(input, { target: { value: "test" } });
+
+    await waitFor(() => {
+      expect(onQuery).toHaveBeenCalledTimes(1);
+      expect(onQuery).toHaveBeenCalledWith("test");
+    });
+  });
+
+  test("should call onQuery when button clicked with the input value", async () => {
+    const onQuery = vi.fn();
+
+    render(<SearchBar onQuery={onQuery} textButton={""} />);
+
+    const input = screen.getByRole("textbox");
+    const button = screen.getByRole("button");
+
+    fireEvent.change(input, { target: { value: "test" } });
+    fireEvent.click(button);
+
+    await waitFor(() => {
+      expect(onQuery).toHaveBeenCalledTimes(1);
+      expect(onQuery).toHaveBeenCalledWith("test");
+    });
+  });
+
+  test("should the input has the correct placeholder value", () => {
+    const placeholder = "Search here";
+
+    render(
+      <SearchBar onQuery={() => {}} textButton={""} placeholder={placeholder} />
+    );
+
+    expect(screen.getByPlaceholderText(placeholder)).toBeDefined();
+  });
+});
